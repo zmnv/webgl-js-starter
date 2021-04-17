49af5cf41620e7111cba560e1a1372414a25e7da
Valeriy Zimnev <sorption.music@gmail.com>
2021-04-17 20:03:09 +0300

    [FIX] Fix start local

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common.js');
-const getLocalIpAddress = require('./getLocalIpAddress.js').default;
+const getLocalIpAddress = require('./getLocalIpAddress');
 const chalk = require('chalk');
 
 // [hicetnunc] ––––––––––––––––––––––––––––––––––––––––––––––––––
